4538aa5192d6395c80fc4f81fc8f77e17b07dca7
Rohini <84307647+20a91a05a0@users.noreply.github.com>
2021-05-29 14:07:54 +0530

    Create Factor.py

---
+++ Factor.py
@@ -0,0 +1,17 @@
+#factors of given number using while
+
+n=int(input('enter number'))
+i=1
+
+While(i<=n)
+ if n%i==0:
+    Print(i)
+  i=i+1
+
+'''
+Expected output:
+  Enter number:9
+   1
+   3
+   9
+'''
