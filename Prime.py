66cccc66c6f6b312a5ac19865231dff00b19225b
Rohini <84307647+20a91a05a0@users.noreply.github.com>
2021-05-29 14:22:59 +0530

    Create Prime.py

---
+++ Prime.py
@@ -0,0 +1,25 @@
+#check whether given number is prime number or not
+n=int(input('enter number')
+i=2
+Count=0
+
+If n<2:
+  Print(invalid number)
+else:
+  While(i<n)
+  If n%i==0:
+    Count+=1
+       Print(%d is not a prime number)
+      Break;
+   i+=1
+    If count==0:
+      Print(%d is a prime number)
+
+'''
+Expected output:
+   Enter number:5
+     5 is a prime number
+
+Enter number:9
+   9 is not a prime number
+'''
